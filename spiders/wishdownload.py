9359533d38320abe17c8af47d70161864bc44627
mike <yangxue088@gmail.com>
2016-03-18 15:30:29 +0800

    Initial commit

---
+++ spiders/wishdownload.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from scrapy_redis.spiders import RedisSpider
+
+from ..items import WfchItem
+
+import re
+
+class WishdownloadSpider(RedisSpider):
+    name = "wishdownload"
+    allowed_domains = ["wish.com"]
+
+    redis_key = 'products'
+
+    def parse(self, response):
+        match = re.search("\['mainContestObj'\] = ({.*?});", response.body)
+        if match:
+            item = WfchItem()
+            item['json'] = match.group(1)
+            return item
