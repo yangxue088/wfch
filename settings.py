035751b09ca74a44acad60270f8e53164f43af20
mike <yangxue088@gmail.com>
2016-03-18 19:12:44 +0800

    disable telnet.

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ NEWSPIDER_MODULE = 'wfch.spiders'
 #COOKIES_ENABLED=False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED=False
+TELNETCONSOLE_ENABLED=False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
