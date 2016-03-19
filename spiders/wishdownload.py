7a320df07ac68a24d3ba88246ed0fbb74dca0c98
mike <yangxue088@gmail.com>
2016-03-19 16:30:08 +0800

    add new parameter to get redis_key.

--- spiders/wishdownload.py
+++ spiders/wishdownload.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import re
 from scrapy_redis.spiders import RedisSpider
 
 from ..items import WfchItem
 
-import re
 
 class WishdownloadSpider(RedisSpider):
     name = "wishdownload"
     allowed_domains = ["wish.com"]
 
-    redis_key = 'products'
+    def __init__(self, redis_key='products'):
+        super(WishdownloadSpider, self).__init__(name, **kwargs)
+        self.redis_key = redis_key
 
     def parse(self, response):
         match = re.search("\['mainContestObj'\] = ({.*?});\n", response.body)
