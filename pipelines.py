6fc19f041eef6b8fb1a2c61162c193ea3c6cca4c
mike <yangxue088@gmail.com>
2016-03-22 22:37:09 +0800

    add new parameter for mongo_collection

--- pipelines.py
+++ pipelines.py
@@ -12,19 +12,22 @@ from pymongo import MongoClient
 
 MONGO_HOST = '127.0.0.1'
 MONGO_PORT = 27017
+MONGO_COLL = 'products'
 
 class WfchPipeline(object):
 
-    def __init__(self, client):
+    def __init__(self, client, coll):
         self.db = client.wish
+        self.coll = coll
 
     @classmethod
     def from_settings(cls, settings):
         host = settings.get('MONGO_HOST', MONGO_HOST)
         port = settings.get('MONGO_PORT', MONGO_PORT)
+        coll = settings.get('MONGO_COLL', MONGO_COLL)
 
         client = MongoClient(host=host, port=port)
-        return cls(client)
+        return cls(client, coll)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -36,5 +39,5 @@ class WfchPipeline(object):
         else:
             data = json.loads(item['json'])
             data['_id'] = data['id']
-            self.db.products.insert_one(data)
+            self.db[self.coll].insert_one(data)
             return item
